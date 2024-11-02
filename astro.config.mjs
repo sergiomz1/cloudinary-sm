19866ca51b51967701a05a6ff90c114e41d49d0b
sergiomz1 <85529375+sergiomz1@users.noreply.github.com>
2024-11-01 19:17:03 -0500

    COMMIT1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import fulldev from 'fulldev-ui/integration';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+  integrations: [
+    fulldev({
+      colors: {
+        theme:'dark',
+        dark: {
+          background: '#1e1e1e',
+          base: '#ffffff',
+          brand: '#f50',
+        }
+      }
+  })
+  ],
+});
