8cb5914cde5e8d623054d3c093e9fd9653e60b33
sergiomz1 <85529375+sergiomz1@users.noreply.github.com>
2024-11-05 16:17:55 -0500

    background

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import fulldev from 'fulldev-ui/integration';
+import fulldev from 'fulldev-ui/integration'
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  integrations: [
-    fulldev({
-      colors: {
-        theme:'dark',
-        dark: {
-          background: '#1e1e1e',
-          base: '#ffffff',
-          brand: '#f50',
-        }
-      }
-  })
-  ],
+    output: 'server',
+    integrations: [
+        fulldev({
+            colors: {
+                theme: 'dark',
+                dark:{
+                    background: '#1f1f1f',
+                    base: '#6f6d66',
+                    brand: '#f50'
+                }
+            }
+        })
+    ]
 });
+
